2bc4ec993454b8ae8d216a3491d209eace43ca72
Ryusei Harada <is0323hi@ed.ritsumei.ac.jp>
2017-11-30 17:57:18 +0900

    精度上げた

--- CropFace.py
+++ CropFace.py
@@ -1,39 +1,31 @@
 import cv2, sys, re
 
-class Crop_Face:
-    """
-    class: Crop_Face
-    cropping cat face
-    """
-    def __init__(self, files):
-        self.files = files
-        if files is None:
-            print("select an image\n")
-            return False
+def Crop_Face(img):
+    if len(img) <= 1:
+        print("no input file\n")
 
+    image_file = img
 
-    def Crop_Face(self, img):
-        if len(img) <= 1:
-            print("no input file\n")
+    cascade_file = "cat_cascade.xml"
+    print("cascade file: " + cascade_file)
 
-        image_file = img
-        output_file = re.sub(r'\.jpg|jpeg|JPG|JPEG|png|PNG$', '-crop.jpg', image_file)
+    image = cv2.imread(image_file)
 
-        cascade_file = "cat_cascade.xml"
-        print("cascade file: " + cascade_file)
+    cascade = cv2.CascadeClassifier(cascade_file)
+    face_images = cascade.detectMultiScale(
+        image,
+        scaleFactor=1.1,
+        minNeighbors=4,
+        minSize=(50, 50)
+        )
 
-        image = cv2.imread(image_file)
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        cascade = cv2.CascadeClassifier(cascade_file)
-        face_images = cascade.detectMultiScale(
-            image_gray,
-            scaleFactor=1.1,
-            minNeighbors=1,
-            minSize=(1, 1))
-
-        if len(face_images) > 0:
-            for (x, y, w, h) in face_images:
-                face_image = image[y:y+h, x:x+w]
+    print(face_images)
+    if len(face_images) > 0:
+        for i, (x, y, w, h) in enumerate(face_images):
+            face_image = image[y:y+h, x:x+w]
+            output_file = re.sub(r'\.jpg|jpeg|JPG|JPEG|png|PNG$', '-'+str(i)+'-crop.jpg', image_file)
             cv2.imwrite(output_file, face_image)
             print(output_file)
+
+    else:
+        print("猫は写ってないみたいだにゃー\n")
