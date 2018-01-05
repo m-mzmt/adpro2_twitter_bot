ca5b8f0dc9ecb87a5815ff45649e14d920e5eb51
m-mzmt <mstk.mzmt@gmail.com>
2018-01-05 14:30:37 +0900

    複数検出した中で最大の画像を保存する修正

--- CropFace.py
+++ CropFace.py
@@ -1,3 +1,4 @@
+import glob
 import cv2, sys, re
 
 class Crop_Face:
@@ -18,22 +19,34 @@ class Crop_Face:
 
         image_file = img
         output_file = re.sub(r'\.jpg|jpeg|JPG|JPEG|png|PNG$', '-crop.jpg', image_file)
-
+        output_file = output_file.replace("./","./crop/")
         cascade_file = "cat_cascade.xml"
         print("cascade file: " + cascade_file)
 
-        image = cv2.imread(image_file)
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        try:
+            image = cv2.imread(image_file)        
+            image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            cascade = cv2.CascadeClassifier(cascade_file)
+            face_images = cascade.detectMultiScale(image_gray,
+                                                   scaleFactor=1.1,
+                                                   minNeighbors=1,
+                                                   minSize=(1, 1))
+            print(face_images)
+            image_len=0
+            if len(face_images) > 0:
+                for (x, y, w, h) in face_images:
+                    face_image = image[y:y+h, x:x+w]
+                    if image_len < w:
+                        image_len = w
+                        face_image = cv2.resize(face_image,(64,64))
+                        print(output_file)
+                        cv2.imwrite(output_file, face_image)
+        except:
+            return
 
-        cascade = cv2.CascadeClassifier(cascade_file)
-        face_images = cascade.detectMultiScale(
-            image_gray,
-            scaleFactor=1.1,
-            minNeighbors=1,
-            minSize=(1, 1))
 
-        if len(face_images) > 0:
-            for (x, y, w, h) in face_images:
-                face_image = image[y:y+h, x:x+w]
-            cv2.imwrite(output_file, face_image)
-            print(output_file)
+if __name__ == '__main__':
+    filepaths = glob.glob("./*.jpg")
+    for p in filepaths:
+        print(p)
+        Crop_Face(p).Crop_Face(p)
